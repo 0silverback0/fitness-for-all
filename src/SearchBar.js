29cf4892fac98a9796f90295838f52ca13c05434
marcell <fourmarz@yahoo.com>
2023-12-30 09:04:52 -0500

    hacky search results bug fix

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import './SearchBar.css'
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [formSubmitted, setFormSubmitted] = useState(false);
-
+  
   const handleSearch = () => {
     if (searchTerm) {
       axios.get(`https://marz.pythonanywhere.com/api/articles/search?title=${searchTerm}`)
@@ -29,6 +29,16 @@ const SearchBar = () => {
     }
   };
 
+  const handleListItemClick = (e) => {
+    e.stopPropagation()
+    const searchResults = document.querySelector('.search-results');
+  if (searchResults) {
+    searchResults.classList.remove('visible');
+    searchResults.classList.add('hidden');
+
+  }
+  };
+
   return (
     <section className="search-bar">
       <div className="container">
@@ -49,13 +59,13 @@ const SearchBar = () => {
         </div>
 
         {formSubmitted && (
-          <div className="search-results">
+          <div className={`search-results ${searchResults.length > 0 ? 'visible' : 'hidden'}`}>
             {searchResults.length > 0 ? (
               <>
                 <h3 className="mb-4">Search Results</h3>
                 <ul className="list-group">
                   {searchResults.map(article => (
-                    <li key={article.id} className="list-group-item">
+                    <li key={article.id} className="list-group-item" onClick={handleListItemClick}>
                       <Link to={`/articles/${article.id}`} className="text-decoration-none">
                         <h4 className="mb-0">{article.title}</h4>
                       </Link>
